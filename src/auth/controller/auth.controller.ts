80669273568f3cca31fee211dea9c9bce6bc8331
aquaperu <aquaperu@outlook.com>
2024-04-11 10:13:56 -0500

    actualizando el controller de auth

--- src/auth/controller/auth.controller.ts
+++ src/auth/controller/auth.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
-
+import { Body, Controller, Get, Param, Post, Req } from '@nestjs/common';
+import { Request } from 'express';
+import { AuthDto } from '../dtos/auth.dto';
+import { AuthService } from '../services/auth.service';
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+  
+    @Post('register')
+    registerUser(@Body() createAuthDto: AuthDto)
+    {
+      
+       return this.authService.create(createAuthDto);
+    }
+    
+    @Post('login')//genera un nuevo token para el cliente
+    loginUserv1(@Req() req:Request)
+    {
+       return this.authService.login(req.body)
+       
+    }
+    @Get('listaauth')
+    lista(){
+      return this.authService.lista()
+
+    }
+    @Get(':token')
+    async buscaUsuario(@Param('token') token: string){
+       return await this.authService.buscarUsuario(token) 
+    }
+}
