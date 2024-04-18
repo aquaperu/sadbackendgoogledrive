4656f98248d8239f33089f7c20e14e3c93894dcb
aquaperu <166466919+aquaperu@users.noreply.github.com>
2024-04-18 09:32:30 -0500

    reparando fix

--- src/valorizacion/services/valorizacion.service.ts
+++ src/valorizacion/services/valorizacion.service.ts
@@ -205,8 +205,8 @@ export class ValorizacionService {
   
     public async generaIndice(indices:INombreColumna[]){ 
        // process.chdir('dist/src/assets')//posisiona el proceso de ejecucion en esta carpeta
-        const pathAssets = fixPathAssets('AmaticSC_Regular.ttf')
-        console.log(pathAssets)
+        //const pathAssets = fixPathAssets('AmaticSC_Regular.ttf')
+        //console.log(pathAssets)
         const indice = new PDFDocument({
             size: "A4"//typePage
         });
