a68291c6999450eb6d08ad7e16e38f6e433dc20c
aquaperu <166466919+aquaperu@users.noreply.github.com>
2024-04-18 09:55:24 -0500

    lo de ficpath

--- src/shared/toolbox/fixPath.ts
+++ src/shared/toolbox/fixPath.ts
@@ -1,6 +1,6 @@
 import  * as path from 'path';
 export const fixPathAssets = (recursoAssets:string)=>{
-    process.chdir('dist/src/assets')
+    //process.chdir('dist/src/assets')
     console.log(process.chdir('dist/src'))
     return `${path.join(process.cwd(),'/',recursoAssets)}`
 }
