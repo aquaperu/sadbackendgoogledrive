617fcbe22532a0b499d39da33075adce1f8b0c18
aquaperu <166466919+aquaperu@users.noreply.github.com>
2024-04-19 22:01:50 +0000

    ruta completa

--- src/shared/toolbox/fixPath.ts
+++ src/shared/toolbox/fixPath.ts
@@ -2,6 +2,6 @@ import  * as path from 'path';
 
 export const fixPathAssets = (recursoAssets:string)=>{
     process.chdir('dist/src/assets')
-    console.log(process.cwd())
-    //return `${path.join(process.cwd(),'/',recursoAssets)}`
+   
+    return `${path.join(process.cwd(),'/',recursoAssets)}`
 }
