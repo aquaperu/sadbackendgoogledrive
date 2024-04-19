bb45f950190a01cdb58b657a26a781f08eafd21e
aquaperu <166466919+aquaperu@users.noreply.github.com>
2024-04-19 21:59:01 +0000

    add  assetes

--- src/shared/toolbox/fixPath.ts
+++ src/shared/toolbox/fixPath.ts
@@ -1,7 +1,7 @@
 import  * as path from 'path';
 
 export const fixPathAssets = (recursoAssets:string)=>{
-    process.chdir('dist/src')
+    process.chdir('dist/src/assets')
     console.log(process.cwd())
     //return `${path.join(process.cwd(),'/',recursoAssets)}`
 }
