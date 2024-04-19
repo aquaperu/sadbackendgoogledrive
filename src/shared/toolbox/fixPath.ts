82807d40b5da3d8e74237a256f0508169bc12e36
aquaperu <166466919+aquaperu@users.noreply.github.com>
2024-04-19 21:54:46 +0000

    carpeta dist

--- src/shared/toolbox/fixPath.ts
+++ src/shared/toolbox/fixPath.ts
@@ -1,7 +1,7 @@
 import  * as path from 'path';
 
 export const fixPathAssets = (recursoAssets:string)=>{
-    process.chdir('dist/src/assets')
-    console.log(process.chdir('dist/src'))
-    return `${path.join(process.cwd(),'/',recursoAssets)}`
+    process.chdir('dist')
+    console.log(process.cwd())
+    //return `${path.join(process.cwd(),'/',recursoAssets)}`
 }
